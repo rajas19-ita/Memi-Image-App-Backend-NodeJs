7bf5573aa0deca5fb767fde5bf8bfc8bf446699a
rajas19-ita <rajaskadge@gmail.com>
2024-11-03 18:37:22 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -8,7 +8,25 @@ import { tagRouter } from "./router/tag.js";
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+    "https://memi-gallery.netlify.app",
+    "http://localhost:5173",
+];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
